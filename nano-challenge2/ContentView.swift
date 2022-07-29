88b16b5d7222a0abdd70d507049ce4cd3fd77d3e
Vincensa Regina Indarto <vincensaregina@gmail.com>
2022-07-29 14:04:23 +0700

    Temp second side bar and content view

--- nano-challenge2/ContentView.swift
+++ nano-challenge2/ContentView.swift
@@ -7,24 +7,50 @@
 
 import SwiftUI
 import CoreData
+import RichTextKit
+
+struct Heading: Hashable {
+    static func == (lhs: Heading, rhs: Heading) -> Bool {
+        return lhs.name == rhs.name
+    }
+    
+    var name: String
+    @State var showSub: Bool = false
+    
+    func toggled() {
+        self.showSub = showSub == true ? false : true
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+        hasher.combine(showSub)
+    }
+}
 
 struct Notes: Hashable {
-    let name: String
-    let heading: [String]
+    var name: String
+    var heading: [Heading]
+    var subheading: [String]
+    var content: String
 }
 
 struct ContentView: View {
-    let notes: [Notes] = [
+    var notes: [Notes] = [
         .init(name: "Mini Challenge 1",
-              heading: ["Market Research", "General Info"]),
-        .init(name: "Nano Challenge 1",
-              heading: ["Swift Syntaxes", "General Info"])
+              heading: [.init(name: "Market Research"), .init(name: "General Info")],
+              subheading: ["Sub 1-1", "Sub 1-2"],
+              content: "Content 1"),
+        .init(name: "Nano Challenge 2",
+              heading: [.init(name: "Swift Syntaxes"), .init(name: "General Info")],
+              subheading: ["Sub 2-1", "Sub 2-2"],
+              content: "Content 2")
     ]
+    
     var body: some View {
         NavigationView {
-            NotesListView()
-            NotesNavigationView()
-            NotesContentView()
+            NotesListView(notes: notes)
+            //            NotesNavigationView()
+            //            NotesContentView()
         }
     }
 }
@@ -36,10 +62,8 @@ struct NotesListView: View {
     //        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
     //        animation: .default)
     //    private var items: FetchedResults<Item>
-    
-    @State private var searchText = ""
-    let notes = ["Mini Challenge 1", "Mini Challenge 2"]
-    
+    @State var notes: [Notes]
+    @State var searchText = ""
     var body: some View {
         VStack {
             Text("Notes").bold().font(.largeTitle).foregroundColor(Color("TitleColor"))
@@ -48,40 +72,45 @@ struct NotesListView: View {
             
             List {
                 ForEach(searchResults, id: \.self) { item in
-                    NavigationLink (destination: Text(item)){
-                        HStack {
-                            Text(item)
-                            Spacer()
-                            Button {
-                            } label: {
-                                Image(systemName: "ellipsis")
+                    ZStack {
+                        NavigationLink (destination: NotesNavigationView(note: item, showSub: false, headingName: "")){
+                            HStack {
+                                Text(item.name)
+                                Spacer()
+                                Button {
+    
+                                } label: {
+                                    Image(systemName: "ellipsis")
+                                }
+                                .buttonStyle(PlainButtonStyle())
                             }
-                            .buttonStyle(PlainButtonStyle())
                         }
+                       
                     }
+                    
+                   
                 }
-                .onDelete(perform: deleteItems)
+                //                .onDelete(perform: deleteItems)
             }
             .searchable(text: $searchText,placement: .sidebar, prompt: "Search")
             
         }
         
         .background(Color("NotesListColor"))
-        .navigationTitle("Notes TTTTT")
-        .toolbar {
-            ToolbarItem {
-                Button(action: addItem) {
-                    Label("Add Item", systemImage: "plus")
-                }
-            }
-        }
+        //        .toolbar {
+        //            ToolbarItem {
+        //                Button(action: addItem) {
+        //                    Label("Add Item", systemImage: "plus")
+        //                }
+        //            }
+        //        }
     }
     
-    var searchResults: [String] {
+    var searchResults: [Notes] {
         if searchText.isEmpty {
             return notes
         } else {
-            return notes.filter { $0.lowercased().contains(searchText) }
+            return notes.filter { $0.name.lowercased().contains(searchText) }
         }
     }
     
@@ -117,16 +146,91 @@ struct NotesListView: View {
 }
 
 struct NotesNavigationView: View {
+    @State var note: Notes
+    @State var showSub: Bool
+    @State var headingName: String
+
     var body: some View {
-        Text("Notes").bold().font(.largeTitle).foregroundColor(Color("TitleColor"))
-            .frame(minWidth: 250, idealWidth: 250, alignment: .leading)
-            .padding(.init(top: 15, leading: 10, bottom: 0, trailing: 0))
+        
+        HStack {
+            VStack {
+                List {
+                    Text("Navigations").font(.body).foregroundColor(Color.gray)
+                        .frame(maxWidth: 250, alignment: .leading)
+                        .padding(.init(top: 0, leading: 0, bottom: -5, trailing: 0))
+                    Text(note.name).bold().font(.largeTitle).foregroundColor(Color("TitleColor"))
+                        .frame(maxWidth: 250, alignment: .leading)
+                        .padding(.bottom)
+                    
+                    ForEach(note.heading, id: \.self) { item in
+                        Button {
+                            showSub.toggle()
+                            headingName = item.name
+                        } label: {
+                            Image(systemName: showSub && headingName == item.name ? "chevron.down" : "chevron.up")
+                            Text(item.name)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        if (showSub && headingName == item.name) {
+                            ForEach(note.subheading, id: \.self) { sub in
+                                
+                                NavigationLink (destination: NotesContentView(note: note)) {
+                                    Text(sub)
+                                    
+                                }
+                                .padding(.init(top: 2, leading: 20, bottom: 0, trailing: 0))
+                                
+                                
+                            }
+                        }
+                        
+                    }
+                }
+                //.onDelete(perform: deleteItems)
+            }
+            
+            
+            Divider()
+            
+            NotesContentView(note: note)
+        }
+        .background(Color.white)
     }
 }
 
 struct NotesContentView: View {
+    var note: Notes
+    var icons = ["bold", "italic", "underline", "list.bullet", "list.number", "textformat", "divider",
+                 "text.alignleft", "text.alignright", "text.aligncenter", "text.justify", "divider", "photo"]
+    @StateObject
+        var context = RichTextContext()
+    @State
+       private var text = NSAttributedString(string: "Type here...")
+    @State private var content: String = ""
     var body: some View {
-        Text("Select an item")
+        VStack {
+            List {
+                HStack {
+                    ForEach(icons, id: \.self) {
+                        item in
+                        Image(systemName: item).padding(.init(top: 0, leading: 10, bottom: 0, trailing: 10))
+                        if (item == "divider") {
+                            Divider().padding(.init(top: 0, leading: 10, bottom: 0, trailing: 10))
+                        }
+                    }
+                }
+                .frame(height: 35, alignment: .topLeading)
+                Divider()
+                TextEditor(text: $content).textSelection(.enabled)
+//                RichTextEditor(text: $text, context: context) {
+//                   // You can customize the native text view here
+//                   $0.textContentInset = CGSize(width: 10, height: 20)
+//                    print("The current font size is \(context.fontSize)")
+//                }.background(Color.red)
+            }
+        }
+        .frame(minWidth: 900, alignment: .leading)
+        .background(Color.white)
     }
 }
 
---
+++ nano-challenge2/RichTextView.swift
@@ -0,0 +1,21 @@
+//
+//  RichTextView.swift
+//  nano-challenge2
+//
+//  Created by Vincensa Regina on 29/07/22.
+//
+
+import SwiftUI
+import AppKit
+
+struct RichTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RichTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        RichTextView()
+    }
+}
